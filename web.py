082f890efd2a551255eb3e66f9231a9aeb2f667e
Niti Goyal <go.nitigoyal@gmail.com>
2024-06-25 14:30:25 -0400

    Collect more property features

--- web.py
+++ web.py
@@ -3,7 +3,26 @@ import requests
 import string
 import pandas as pd
 
-df = pd.DataFrame()
+#Disable SSL certificate verification warnings
+requests.packages.urllib3.disable_warnings()
+
+df = pd.DataFrame(columns=["Location", "Acct#", "Current Assessment", "Building Count", 
+                           "Mblu",  "PID", "Owner", "Co-Owner", "Owner Address", 
+                           "Sale Price", "Certificate", "Book & Page", "Sale Date", 
+                           "Instrument", "Ownership History", "Year Built", 
+                           "Living Area", "Replacement Cost", "Building Percent Good", 
+                           "Replacement Cost Less Depreciation", "Building Attributes", 
+                           "Building Sub-Areas","Extra Features","Current Valuation",
+                            "Land Use Code", "Land Description",
+                             "Land Zone", "Neighborhood", "Alt Land Appr", "Land Category",
+                             "Land Size (Sqr Feet)", "Land Frontage", "Land Depth",
+                            "Land Assessed Value", "Outbuildings", "Valuation History"
+                           ])
+small_df = pd.DataFrame(columns=["Location", "Acct#", "Current Assessment", "Building Count", 
+                           "Mblu",  "PID", "Owner", "Co-Owner", "Owner Address", 
+                           "Sale Price", "Certificate", "Book & Page", "Sale Date", 
+                           "Instrument", "Ownership History", "Year Built"])
+counter = 0
 
 #Function to remove links that don't lead to a street page
 def has_street_name(link):
@@ -58,4 +77,139 @@ for i in street_letters:
       house_pid = anchor_tag["href"] #anchor_tag["href"] returns the pid part of the tag (our URL route)
       houseURL = f"https://gis.vgsi.com/lexingtonma/{house_pid}"
       house_page = requests.get(houseURL, verify=False)
-      house_html_soup = BeautifulSoup(house_page.content, "html.parser")
+      soup = BeautifulSoup(house_page.content, "html.parser")
+
+      #Parse the page of the house
+      row = {}
+      location = soup.find(id="MainContent_lblLocation").get_text()
+      print(location)
+      acct = soup.find(id="MainContent_lblAcctNum").get_text()
+      assessment = soup.find(id="MainContent_lblGenAssessment").get_text()
+      bld_count = soup.find(id="MainContent_lblBldCount").get_text()
+      mblu = pid = soup.find(id="MainContent_lblMblu").get_text()
+      pid = soup.find(id="MainContent_lblPid").get_text()
+      owner = soup.find(id="MainContent_lblGenOwner").get_text()
+      co_owner = soup.find(id="MainContent_lblCoOwner").get_text()
+      owner_address = soup.find(id="MainContent_lblAddr1").get_text()
+      sl_price = soup.find(id="MainContent_lblPrice").get_text()
+      cert = soup.find(id="MainContent_lblCertificate").get_text()
+      bp = soup.find(id="MainContent_lblBp").get_text()
+      sl_date = soup.find(id="MainContent_lblSaleDate").get_text()
+      ins = soup.find(id="MainContent_lblInstrument")
+      ins = ins.get_text() if ins else None
+
+      own_hist = pd.DataFrame()
+      own_hist_table = soup.find("table", id="MainContent_grdSales")
+      if own_hist_table:
+        #Get the table's column headers
+        header_tags = own_hist_table.find("tr", class_ = "HeaderStyle")
+        #extract just the th tags
+        header_tags = header_tags.find_all("th", scope="col")
+        for header in header_tags:
+          #Add header (e.g. "Instrument") to the dataframe
+          own_hist[header.get_text()] = None
+        white_rows = own_hist_table.find_all("tr", class_ = "RowStyle")
+        for white_row in white_rows:
+          white_td_tags = white_row.find_all("td")
+          new_row = {}
+          for column, tag in zip(own_hist.columns, white_td_tags):
+            text = tag.get_text()
+            if "&nbsp" not in text:
+              new_row[column] = text
+          own_hist = own_hist._append(new_row, ignore_index=True)
+          #print(new_row)
+        gray_rows = own_hist_table.find_all("tr", class_ = "AltRowStyle")
+        for gray_row in gray_rows:
+          gray_td_tags = gray_row.find_all("td")
+          new_row = {}
+          for column, tag in zip(own_hist.columns, gray_td_tags):
+            text = tag.get_text()
+            if "&nbsp" not in text:
+              new_row[column] = text
+          own_hist = own_hist._append(new_row, ignore_index=True)
+
+      yr_blt = soup.find(id="MainContent_ctl01_lblYearBuilt").get_text()
+      lvg_area = soup.find(id="MainContent_ctl01_lblBldArea").get_text()
+      rplce_cost = soup.find(id="MainContent_ctl01_lblRcn").get_text()
+      bld_pc_good = soup.find(id="MainContent_ctl01_lblPctGood").get_text()
+      rplce_cost_lsdep = soup.find(id="MainContent_ctl01_lblRcnld").get_text()
+
+      bldg_attr = {}
+      bldg_attr_table = soup.find("table", id="MainContent_ctl01_grdCns")
+      if bldg_attr_table:
+        white_rows = bldg_attr_table.find_all("tr", class_ = "RowStyle")
+        for white_row in white_rows:
+          white_td_tags = white_row.find_all("td")
+          style, description = white_td_tags
+          style_text = style.get_text()
+          description_text=description.get_text()
+          if "&nbsp" not in description_text:
+            bldg_attr[style_text] = description_text
+        gray_rows = bldg_attr_table.find_all("tr", class_ = "AltRowStyle")
+        for gray_row in gray_rows:
+          gray_td_tags = gray_row.find_all("td")
+          style, description = gray_td_tags
+          style_text = style.get_text()
+          description_text=description.get_text()
+          if "&nbsp" not in description_text:
+            bldg_attr[style_text] = description_text
+
+      #bld_sub_areas = ins.get_text() if ins else None
+      #extra_fts = 
+      ld_use_code = soup.find(id="MainContent_lblUseCode").get_text()
+      ld_description = soup.find(id="MainContent_lblUseCodeDescription").get_text()
+      ld_zone = soup.find(id="MainContent_lblZone").get_text()
+      nbhd = soup.find(id="MainContent_lblNbhd").get_text()
+      alt_ld_appr = soup.find(id="MainContent_lblAltApproved").get_text()
+      ld_ctg = soup.find(id="MainContent_lblLndCategory").get_text()
+      ld_size = soup.find(id="MainContent_lblLndSf").get_text()
+      ld_frontage = soup.find(id="MainContent_lblLndFront").get_text()
+      ld_depth = soup.find(id="MainContent_lblDepth").get_text()
+      ld_value = soup.find(id="MainContent_lblLndAsmt").get_text()
+                  
+      row["Location"] = location
+      row["Acct#"] = acct
+      row["Current Assessment"] = assessment
+      row["Building Count"] = bld_count
+      row["Mblu"] = mblu
+      row["PID"] = pid
+      row["Owner"] = owner
+      row["Co-Owner"] = co_owner
+      row["Owner Address"] = owner_address
+      row["Sale Price"] = sl_price
+      row["Certificate"] = cert
+      row["Book & Page"] = bp
+      row["Sale Date"] = sl_date
+      row["Instrument"] = ins
+      row["Ownership History"] = own_hist
+      row["Year Built"] = yr_blt
+      row["Living Area"] = lvg_area
+      row["Replacement Cost"] = rplce_cost
+      row["Building Percent Good"] = bld_pc_good
+      row["Replacement Cost Less Depreciation"] = rplce_cost_lsdep
+      row["Building Attributes"] = bldg_attr
+      row["Land Use Code"] = ld_use_code
+      row["Land Description"] = ld_description
+      row["Land Zone"] = ld_zone
+      row["Neighborhood"] = nbhd
+      row["Alt Land Appr"] = alt_ld_appr
+      row["Land Category"] = ld_ctg
+      row["Land Size (Sqr Feet)"] = ld_size
+      row["Land Frontage"] = ld_frontage
+      row["Land Depth"] = ld_depth
+      row["Land Assessed Value"] = ld_value
+      row["Building Sub-Areas"] = NotImplemented
+      row["Extra Features"] = NotImplemented
+
+
+
+      small_df = small_df._append(row, ignore_index=True)
+      counter+=1
+      if counter==15:
+        break
+    if counter==15:
+      break
+  if counter==15:
+    break
+
+small_df.to_csv("output.csv")
