7cb22fbc58c3c086bb58301cdcc637fd4c87a465
Niti Goyal <go.nitigoyal@gmail.com>
2024-08-04 12:58:46 -0400

    Add features list to scraping file

--- scrape_property_data.py
+++ scrape_property_data.py
@@ -6,7 +6,19 @@ import pandas as pd
 import time
 from concurrent import futures
 from threading import Lock
-from features import features_lst
+
+features_lst = ["Location", "Acct#", "Current Assessment", "Building Count", 
+                           "Mblu",  "PID", "Owner", "Co-Owner", "Owner Address", 
+                           "Sale Price", "Certificate", "Book & Page", "Sale Date", 
+                           "Instrument", "Ownership History", "Year Built", 
+                           "Living Area", "Replacement Cost", "Building Percent Good", 
+                           "Replacement Cost Less Depreciation", "Building Attributes", 
+                           "Building Sub-Areas","Extra Features",
+                            "Land Use Code", "Land Description",
+                             "Land Zone", "Neighborhood", "Alt Land Appr", "Land Category",
+                             "Land Size (Sqr Feet)", "Land Frontage", "Land Depth",
+                            "Land Assessed Value", "Outbuildings", "Valuation History"
+                           ]
 
 df = pd.DataFrame(columns=features_lst)
 lock = Lock() #each thread must aquire the lock to add a house row to the global dataframe
